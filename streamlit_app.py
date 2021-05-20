7fb42b49c30497be87fc78a7bc8a50679c8e86be
Ryan <2190424@slu.edu.ph>
2021-05-20 21:18:49 +0800

    Added test class

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,93 @@ import matplotlib as plt
 import seaborn as sns
 import pickle
 
+# Classifer Library
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics.pairwise import cosine_similarity
+import category_encoders as ce
+
 # Load learned model
 # loaded_model = pickle.load(open('model.pkl', 'rb'))
 # loaded_encoder = pickle.load(open('encoder.pkl', 'rb'))
+
+# ============================================================
+# Result Function : Random Forest Implementation
+def predict():
+    df = pd.read_csv('clean_data.csv')
+    df = df.loc[df['Current contraceptive method'] != 'Not using']
+    df['Current contraceptive method'] = df['Current contraceptive method'].replace('Calendar or rhythm method/Periodic abstinence', 'Periodic abstinence', regex=True)
+    df['Current contraceptive method'] = df['Current contraceptive method'].replace('Implants/Norplant', 'Implants', regex=True)
+    df['Current contraceptive method'] = df['Current contraceptive method'].replace('Mucus/Billing/Ovulation', 'Ovulation', regex=True)
+
+    columns = ["Respondent's current age",
+                'Age of respondent at 1st birth',
+                'Age at first menstrual period',
+                'Recent sexual activity',
+                'Region',
+                'Type of place of residence',
+                'Current marital status',
+                'Births in last five years',
+                'Births in last three years',
+                'Births in past year',
+                'Currently pregnant',
+                'Total number all pregnacies',
+                'Decision maker for using contraception',
+                'Decision maker for not using contraception',
+                'Preferred future method',
+                'Smokes cigarettes',
+                'Smokes pipe full of tobacco',
+                'Chews tobacco',
+                'Snuffs by nose',
+                'Smokes kreteks',
+                'Smokes cigars, cheroots or cigarillos',
+                'Smokes water pipe',
+                'Snuff by mouth',
+                'Chews betel quid with tobacco',
+                "Husband's desire for children",
+                'Exposure',
+                'Unmet need',
+                'Unmet need (definition 2)',
+                'Unmet need for contraception (definition 3)'
+                ]
+    X = df[columns]
+    y = df['Current contraceptive method']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+
+    X_encoder = ce.OneHotEncoder(cols=[
+        'Recent sexual activity',
+        'Region',
+        'Type of place of residence',
+        'Current marital status',
+        'Currently pregnant',
+        'Decision maker for using contraception',
+        'Decision maker for not using contraception',
+        'Preferred future method',
+        'Smokes cigarettes',
+        'Smokes pipe full of tobacco',
+        'Chews tobacco',
+        'Snuffs by nose',
+        'Smokes kreteks',
+        'Smokes cigars, cheroots or cigarillos',
+        'Smokes water pipe',
+        'Snuff by mouth',
+        'Chews betel quid with tobacco',
+        "Husband's desire for children",
+        'Exposure',
+        'Unmet need',
+        'Unmet need (definition 2)',
+        'Unmet need for contraception (definition 3)'
+    ])
+
+    X_train = X_encoder.fit_transform(X_train)
+    X_test = X_encoder.transform(X_test)
+
+    rf = RandomForestClassifier(n_estimators=100)
+    rf.fit(X_train, y_train)
+    st.write(X_test)
 
 st.write("""
 # System Web Application Version
@@ -359,12 +443,9 @@ with st.form("Counseling_Form"):
     submit_button = st.form_submit_button(label="Submit Information")
     if submit_button:
         st.write("Predicting/Recommending Contraceptive...")
-        st.write({unmet_need_1})
+        # st.write({unmet_need_1})
+        predict()
 
-
-
-# ============================================================
-# Result Function
 
 
 # ============================================================
