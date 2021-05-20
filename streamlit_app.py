81ba00fd2ecbdacbad75e2d4cd1127cddb008f83
Ryan <2190424@slu.edu.ph>
2021-05-20 18:13:14 +0800

    Test input

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,37 @@
+# Dependencies and Libraries
 import streamlit as st
+import numpy as np
 import pandas as pd
+import sklearn as skl
+import matplotlib as plt
+import seaborn as sns
 
 st.write("""
-# Streamlit Demo App
-Shown are the stock closing price and volume of Google!
+# System Web Application Version
+A [CS 321 | CS 322] Project
 """)
+
+# Residential Status
+residential_status = st.selectbox(
+    label="Region", 
+    options=[
+        'Autonomous Region in Muslim Mindanao', 
+        'Zamboanga Peninsula', 
+        'Soccskargen', 
+        'Cordillera', 
+        'Caraga',
+        'National Capital', 
+        'Western Visayas', 
+        'Central Visayas', 
+        'Ilocos', 
+        'Cagayan Valley', 
+        'Central Luzon',
+        'Calabarzon', 
+        'Mimaropa', 
+        'Bicol', 
+        'Eastern Visayas', 
+        'Northern Mindanao', 
+        'Davao'
+        ],
+    index=0
+    )
