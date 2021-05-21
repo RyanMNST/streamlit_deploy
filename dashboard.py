8d9aacccc2d4ef429b2bac2fc6f0189d60939fa1
2193079 <55934659+2193079@users.noreply.github.com>
2021-05-21 17:50:35 +0800

    minor edits, still not working

--- dashboard.py
+++ dashboard.py
@@ -7,15 +7,15 @@ from pathlib import Path
 @st.cache(allow_output_mutation=True)
 def read_data():
     m_path = Path(__file__).parent
-    data_path = m_path.joinpath('dataset/clean_data.csv')
+    data_path = m_path.joinpath("dataset/clean_data.csv")
     df = pd.read_csv(data_path, index_col=0)
-    df['Current contraceptive method'] = df['Current contraceptive method'].replace('Calendar or rhythm method/Periodic abstinence', 'Periodic abstinence', regex=True)
-    df['Current contraceptive method'] = df['Current contraceptive method'].replace('Implants/Norplant', 'Implants', regex=True)
-    df['Current contraceptive method'] = df['Current contraceptive method'].replace('Mucus/Billing/Ovulation', 'Ovulation', regex=True)
+    df["Current contraceptive method"] = df["Current contraceptive method"].replace("Calendar or rhythm method/Periodic abstinence", "Periodic abstinence", regex=True)
+    df["Current contraceptive method"] = df["Current contraceptive method"].replace("Implants/Norplant", "Implants", regex=True)
+    df["Current contraceptive method"] = df["Current contraceptive method"].replace("Mucus/Billing/Ovulation", "Ovulation", regex=True)
 
-    df['Preferred future method'] = df['Preferred future method'].replace('Calendar or rhythm method/Periodic abstinence', 'Periodic abstinence', regex=True)
-    df['Preferred future method'] = df['Preferred future method'].replace('Implants/Norplant', 'Implants', regex=True)
-    df['Preferred future method'] = df['Preferred future method'].replace('Mucus/Billing/Ovulation', 'Ovulation', regex=True)
+    df["Preferred future method"] = df["Preferred future method"].replace("Calendar or rhythm method/Periodic abstinence", "Periodic abstinence", regex=True)
+    df["Preferred future method"] = df["Preferred future method"].replace("Implants/Norplant", "Implants", regex=True)
+    df["Preferred future method"] = df["Preferred future method"].replace("Mucus/Billing/Ovulation", "Ovulation", regex=True)
     return df
 
 def pairplot(data, vars=None):
@@ -23,9 +23,9 @@ def pairplot(data, vars=None):
         vars = list(data.columns)
 
     chart = alt.Chart(data).mark_circle().encode(
-                alt.X(alt.repeat("column"), type='quantitative'),
-                alt.Y(alt.repeat("row"), type='quantitative'),
-                color='Origin:N'.format(hue=hue)
+                alt.X(alt.repeat("column"), type="quantitative"),
+                alt.Y(alt.repeat("row"), type="quantitative"),
+                color="Origin:N"
             ).properties(
                 width=250,
                 height=250
@@ -33,7 +33,7 @@ def pairplot(data, vars=None):
                 row=vars,
                 column=vars
             ).interactive()
-    return chart 
+    return chart
 
 def app():
     st.markdown(
@@ -66,62 +66,63 @@ def app():
     st.write(data.sample(n=25))
 
     features = ["Respondent's current age",
-    'Age of respondent at 1st birth',
-    'Age at first menstrual period',
-    'Recent sexual activity',
-    'Region',
-    'Type of place of residence',
-    'Current marital status',
-    'Births in last five years',
-    'Births in last three years',
-    'Births in past year',
-    'Currently pregnant',
-    'Total number all pregnacies',
-    'Current contraceptive method',
-    'Decision maker for using contraception',
-    'Decision maker for not using contraception',
-    'Preferred future method',
-    'Smokes cigarettes',
-    'Smokes pipe full of tobacco',
-    'Chews tobacco',
-    'Snuffs by nose',
-    'Smokes kreteks',
-    'Smokes cigars, cheroots or cigarillos',
-    'Smokes water pipe',
-    'Snuff by mouth',
-    'Chews betel quid with tobacco',
+    "Age of respondent at 1st birth",
+    "Age at first menstrual period",
+    "Recent sexual activity",
+    "Region",
+    "Type of place of residence",
+    "Current marital status",
+    "Births in last five years",
+    "Births in last three years",
+    "Births in past year",
+    "Currently pregnant",
+    "Total number all pregnacies",
+    "Current contraceptive method",
+    "Decision maker for using contraception",
+    "Decision maker for not using contraception",
+    "Preferred future method",
+    "Smokes cigarettes",
+    "Smokes pipe full of tobacco",
+    "Chews tobacco",
+    "Snuffs by nose",
+    "Smokes kreteks",
+    "Smokes cigars, cheroots or cigarillos",
+    "Smokes water pipe",
+    "Snuff by mouth",
+    "Chews betel quid with tobacco",
     "Husband's desire for children",
-    'Exposure',
-    'Unmet need',
-    'Unmet need (definition 2)',
-    'Unmet need for contraception (definition 3)'
+    "Exposure",
+    "Unmet need",
+    "Unmet need (definition 2)",
+    "Unmet need for contraception (definition 3)"
     ]
 
-    examine_expander = st.beta_expander(label='Examine the Dataset')
+    examine_expander = st.beta_expander(label="Examine the Dataset")
     with examine_expander:
         col1, col2 = st.beta_columns(2)
 
         chosen_feature = col1.selectbox("Select the feature of the dataset to examine", tuple(features))
-        col1.write('Value Counts')
+        col1.write("Value Counts")
         col1.write(data[chosen_feature].value_counts().sort_index(ascending=True))
 
         col2.write("Chart")
         col2.bar_chart(data[chosen_feature].value_counts(), width=0, height=0)
 
-    paper_expander = st.beta_expander(label='Graphs from the Paper')
+    paper_expander = st.beta_expander(label="Graphs from the Paper")
     with paper_expander:
-        for column_name in data.columns:
-            if data[column_name].dtypes == np.object:
-                data[column_name] = data[column_name].astype('category')
-                data[column_name] = data[column_name].cat.codes
-                data[column_name] = data[column_name].astype(int)
+        with st.form("Graphs"):
+            for column_name in data.columns:
+                if data[column_name].dtypes == np.object:
+                    data[column_name] = data[column_name].astype("category")
+                    data[column_name] = data[column_name].cat.codes
+                    data[column_name] = data[column_name].astype(int)
 
-        col1, col2 = st.beta_columns(2)
+            col1, col2 = st.beta_columns(2)
 
-        pairplot = col1.button('Pair plot relationship (Age Features - Pregnancy Features)')
-        heatmap = col1.button('Heatmap (Age - Maternal History)')
+            pairplot_button = col1.form_submit_button(label="Pair plot relationship (Age Features - Pregnancy Features)")
+            heatmap = col1.form_submit_button(label="Heatmap (Age - Maternal History)")
 
-        if pairplot:
-            col2.altair_chart(
-                pairplot(data, vars=["Respondent's current age", 'Age at first sex','Age of respondent at 1st birth','Total number all pregnacies','Births in last five years','Births in last three years','Births in past year']))
-
+            if pairplot_button:
+                col2.altair_chart(
+                    pairplot(data, vars=["Respondent's current age", "Age at first sex","Age of respondent at 1st birth","Total number all pregnacies","Births in last five years","Births in last three years","Births in past year"])
+                )
