0306222b615a4efb00a683584c1b82ff0f677464
2193079 <55934659+2193079@users.noreply.github.com>
2021-05-21 15:08:28 +0800

    sorting by index

--- dashboard.py
+++ dashboard.py
@@ -4,6 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
 
+@st.cache
+def read_data():
+    m_path = Path(__file__).parent
+    data_path = m_path.joinpath('dataset/clean_data.csv')
+    return pd.read_csv(data_path, index_col=0)
+
 def app():
     st.markdown(
         f"""
@@ -29,10 +35,7 @@ def app():
     A [CS 321 | CS 322] Project - Dataset Dashboard
     """)
 
-    m_path = Path(__file__).parent
-    data_path = m_path.joinpath('dataset/clean_data.csv')
-
-    data = pd.read_csv(data_path, index_col=0)
+    data = read_data()
 
     st.markdown("**Sample of the dataset**")
     st.write(data.head(25))
@@ -71,10 +74,10 @@ def app():
     chosen_feature = st.selectbox("Select the feature of the dataset to examine", tuple(features))
     col1, col2 = st.beta_columns(2)
 
-    col1.write('Unique Values')
-    col1.write(data[chosen_feature].unique())
-    # col1.write('Value Counts')
-    # col1.write(data[chosen_feature].value_counts().sort_values(ascending=False))
+    # col1.write('Unique Values')
+    # col1.write(data[chosen_feature].unique())
+    col1.write('Value Counts')
+    col1.write(data[chosen_feature].value_counts().sort_index(ascending=True))
 
     col2.write("Chart")
     col2.bar_chart(data[chosen_feature].value_counts(), width=0, height=0)
